81a418c686ff2c7cd3038d64127803fb68e2ff77
parzival68 <123883153+parzival68@users.noreply.github.com>
2023-04-06 11:30:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <image src={logo} alt="logo"  />
+          <image src={} alt="logo"  />
         </Link>
         <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
       </header>
