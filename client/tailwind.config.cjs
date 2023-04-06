965455636c0f349fb62b77b35e6364f7afa91c50
parzival68 <123883153+parzival68@users.noreply.github.com>
2023-04-06 11:36:30 +0530

    Update tailwind.config.cjs

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -10,6 +10,9 @@ module.exports = {
       fontFamily: {
         inter: ['Inter var', 'sans-serif'],
       },
+       images: {
+        domains: ['links.papareact.com'],
+      },
       boxShadow: {
         card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
         cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
