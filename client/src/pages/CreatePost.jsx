8d11864885e103fcb9bc2352535cf341731d2c57
parzival68 <123883153+parzival68@users.noreply.github.com>
2023-04-06 11:23:46 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -106,7 +106,7 @@ const CreatePost = () => {
     <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images through Dall-E AI and share them with the community</p>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images through AI Image Generator and share them with the community</p>
       </div>
 
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
